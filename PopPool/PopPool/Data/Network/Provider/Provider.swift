7b9317b4dffd59035a32c0d5c74c26b4b6e3cf4b
dongglehada <ghddns34@gmail.com>
2024-06-04 15:12:47 +0900

    [REFACTOR] : ♻️ RxAlamofire 적용

--- PopPool/PopPool/Data/Network/Provider/Provider.swift
+++ PopPool/PopPool/Data/Network/Provider/Provider.swift
@@ -7,93 +7,73 @@
 
 import Foundation
 import RxSwift
+import RxAlamofire
 
 protocol Provider {
-    func requestData<R: Decodable, E: RequesteResponsable>(with endpoint: E) -> Observable<R>
+    /// 네트워크 요청을 수행하고 결과를 반환하는 메서드
+    /// - Parameter endpoint: 요청 엔드포인트
+    /// - Returns: 요청에 대한 결과를 Single로 반환
+    func requestData<R: Decodable, E: RequesteResponsable>(with endpoint: E) -> Single<R> where R == E.Response
 }
 
 class ProviderImpl: Provider {
     
-    let session: URLSessionable
-    let disposeBag = DisposeBag()
-    init(session: URLSessionable = URLSession.shared) {
-        self.session = session
-    }
-    
-    /// 서버에 데이터 요청을 보내는 메서드입니다
-    /// - Parameter endpoint: callsite에서 DTO로 구성된 Endpoint 객체를 받습니다
-    /// - Returns: Observable Data
-    func requestData<R, E>(with endpoint: E) -> Observable<R> where R: Decodable, E: RequesteResponsable {
-        return Observable.create { [weak self] observer in
-            guard let self = self else {
-                return Disposables.create()
-            }
+    func requestData<R: Decodable, E: RequesteResponsable>(with endpoint: E) -> Single<R> where R == E.Response {
+        
+        return Single.create { single in
             
             do {
                 let urlRequest = try endpoint.getUrlRequest()
                 
-                self.session.dataTask(with: urlRequest)
-                    .flatMap { (response, data) -> Observable<Result<Data, Error>> in
-                        return self.checkError(with: data, response, nil)
+                let request = RxAlamofire.requestData(urlRequest)
+                    .flatMap { (response, data) -> Single<Data> in
+                        return self.checkError(with: data, response)
                     }
-                    .subscribe(onNext: { result in
-                        switch result {
-                        case .success(let validData):
-                            do {
-                                let decodedData = try JSONDecoder().decode(R.self, from: validData)
-                                observer.onNext(decodedData)
-                                observer.onCompleted()
-                            } catch {
-                                observer.onError(error)
-                            }
-                        case .failure(let error):
-                            observer.onError(error)
-                        }
-                    }, onError: { error in
-                        observer.onError(error)
-                    })
-                    .disposed(by: self.disposeBag)
-                
+                    .flatMap { data -> Single<R> in
+                        return self.decodeData(data: data)
+                    }
+                    .subscribe { decodeData in
+                        single(.success(decodeData))
+                    } onError: { error in
+                        single(.failure(error))
+                    }
+                return Disposables.create {
+                    request.dispose()
+                }
             } catch {
-                observer.onError(NetworkError.unknownError)
+                single(.failure(NetworkError.urlRequest(error)))
             }
-            
             return Disposables.create()
         }
     }
     
-    
-    /// Reactive Programming인 RxSwift를 활용하면서 생성된 메서드입니다
-    /// session.dataTask(with: urlRequest)에서 발생하는 결과값을 처리하기 위한 역할
-    /// trailing closure로 처리하던 data, response, error를 관리합니다
+    /// HTTP 응답 상태 코드 확인 및 처리
     /// - Parameters:
-    ///   - data: urlRequest의 응답 값을 담고 있습니다
-    ///   - response: urlRequest의 응답 값을 담고 있습니다
-    ///   - error: urlRequest의 응답 값을 담고 있습니다
-    /// - Returns: Observable Data
-    private func checkError(with data: Data?, _ response: URLResponse?, _ error: Error?) -> Observable<Result<Data,Error>> {
-        return Observable.create { observer in
-            if let error = error {
-                observer.onNext(.failure(error))
-                observer.onCompleted()
-                return Disposables.create()
+    ///   - data: 응답 데이터
+    ///   - response: HTTP 응답
+    /// - Returns: 응답 데이터를 Single로 반환
+    private func checkError(with data: Data, _ response: HTTPURLResponse) -> Single<Data> {
+        return Single<Data>.create { single in
+            if (200...299).contains(response.statusCode) {
+                single(.success(data))
+            } else {
+                single(.failure(NetworkError.invalidHttpStatusCode(response.statusCode)))
             }
-            
-            guard let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
-                observer.onNext(.failure(NetworkError.urlResponse))
-                observer.onCompleted()
-                return Disposables.create()
+            return Disposables.create()
+        }
+    }
+    
+    /// JSON 데이터 디코딩 처리
+    /// - Parameter data: 디코딩할 데이터
+    /// - Returns: 디코딩된 데이터를 Single로 반환
+    private func decodeData<R: Decodable>(data: Data) -> Single<R> {
+        return Single.create { single in
+            do {
+                let decodeData = try JSONDecoder().decode(R.self, from: data)
+                single(.success(decodeData))
+            } catch {
+                single(.failure(error))
             }
-            
-            guard let data = data else {
-                observer.onNext(.failure(NetworkError.emptyData))
-                observer.onCompleted()
-                return Disposables.create()
-            }
-            
-            observer.onNext(.success(data))
-            observer.onCompleted()
-            
             return Disposables.create()
         }
     }
--- PopPool/PopPool/Presentation/CodeConvention Sample/ViewControllerViewModel.swift
+++ PopPool/PopPool/Presentation/CodeConvention Sample/ViewControllerViewModel.swift
@@ -20,6 +20,8 @@ final class ViewControllerViewModel: ViewModel {
         var loadCount: BehaviorRelay<Int>
     }
     
+    var provider = ProviderImpl()
+    
     // MARK: - Properties
 
     var disposeBag = DisposeBag()
@@ -33,11 +35,25 @@ final class ViewControllerViewModel: ViewModel {
         input.didTapButton.emit { [weak self] _ in
             guard let self = self else { return }
             self.count.accept(self.count.value + 1)
+            testProvider()
         }
         .disposed(by: disposeBag)
         
         return Output(
             loadCount: self.count
         )
+    }
+    
+    func testProvider() {
+        let requestDTO = TestRequestDTO(query: "cat")
+        let endpoint = APIEndpoint.fetchData(with: requestDTO)
+        
+        provider.requestData(with: endpoint)
+            .subscribe { data in
+                print(data)
+            } onFailure: { error in
+                print(error)
+            }
+            .disposed(by: disposeBag)
     }
 }
