37a53e042fc0f746d9dc444e8154bc87a89b90c5
7과11사이 <dongjunlee222@gmail.com>
2024-06-10 13:14:41 +0900

    [DOCS]: 📚 Coordinator Pattern 주석 추가

--- PopPool/PopPool/Presentation/Coordinator/AppCoordinator.swift
+++ PopPool/PopPool/Presentation/Coordinator/AppCoordinator.swift
@@ -7,16 +7,39 @@
 
 import UIKit
 
+/// 앱의 초기 Coordinator 연결부입니다
 class AppCoordinator: BaseCoordinator {
     
+    // MainCoordinator를 연결합니다
     override func start() {
+        
+        /// BaseCoordinator를 적용하지 않는 이유
+            // BaseCoordinator의 역할은 메서드들을 일일히 선언하지 않는 편의성을 위함입니다
+            // AppCoordinator의 start() 메서드 안에 BaseCoordinator를 담을 경우
+            // BaseCoordinator 자체는 값이 없기 때문에 화면이 보이지 않게 됩니다.
+            
+            // 또한 BaseCoordinator에는 '화면 전환' 메서드를 가지고 있지에
+            // baseCoordinator를 한번 더 감싼 MainCoordinator를 적용합니다
+              
+        /// BaseCoordinator에 rootViewController를 선언하지 않는 이유
+            // 아래와 같이 코드를 직접 선언할 경우, root.coordinator 대리자 문제가 발생합니다
+            // AppCoordinator > MainCoordinator > BaseCoordinator 인 구조이기에
+            // 최상위 coordinator 역할을 App에게 위임할 경우,
+            // ViewController가 가지고 있는 coordinator 델리게이트 역할을 위임 시킬 수 있는 객체가 없습니다
+                  
+            // let rootViewModel = ViewControllerViewModel()
+            // let root = ViewController(viewModel: rootViewModel)
+            // root.coordinator = self
+
         let coordinator = MainCoordinator(navigationController: navigationController)
         moveToChild(coordinator: coordinator)
     }
 }
 
+/// 첫 화면의 transition을 담당하는 MainCoordinator입니다
 class MainCoordinator: BaseCoordinator {
     
+    // 지정한 ViewController를 navigationConstroller로 올려둡니다
     override func start() {
         let rootViewModel = ViewControllerViewModel()
         let rootVC = ViewController(viewModel: rootViewModel)
@@ -24,6 +47,7 @@ class MainCoordinator: BaseCoordinator {
         navigationController.pushViewController(rootVC, animated: false)
     }
     
+    // 지정한 ViewController로 이동합니다
     func moveToSecondScreen() {
         removeChildCoordinators()
         let coordinator = ChildrenCoordinator(navigationController: navigationController)
--- PopPool/PopPool/Presentation/Coordinator/Coordinator.swift
+++ PopPool/PopPool/Presentation/Coordinator/Coordinator.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+/// Coordinator의 기본 구성입니다
 protocol Coordinator: AnyObject {
     var navigationController: UINavigationController { get set }
     var parentCoordinator: Coordinator? { get set }
@@ -16,6 +17,8 @@ protocol Coordinator: AnyObject {
     func removeChildCoordinators()
 }
 
+/// Coordinator를 채택한 BaseCoordinator입니다
+/// 모든 Coordinator는 해당 Base를 채택하여 적용하면 됩니다
 class BaseCoordinator: Coordinator {
     
     // MARK: - Properties
@@ -34,16 +37,20 @@ class BaseCoordinator: Coordinator {
     
     // MARK: - Methods
     
+    // coordinator를 연결 짓기 위한 필수 메서드입니다
+    // override func start()로 사용해주세요
     func start() {
         print("코디네이터 연결이 필요합니다")
     }
     
+    // 하위 Coordinator를 연결합니다
     func moveToChild(coordinator: Coordinator) {
         childCoordinator.append(coordinator)
         coordinator.parentCoordinator = self
         coordinator.start()
     }
     
+    // 하위 Coordinator를 제거합니다
     func removeChildCoordinators() {
         childCoordinator.forEach { $0.removeChildCoordinators() }
         childCoordinator.removeAll()
