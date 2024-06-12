94fe40f44134d7a37464be1ec369342d91e7dded
Madman-dev <dongjunlee222@gmail.com>
2024-06-13 02:12:15 +0900

    [DOCS]: 📚 Coordinator 관련 주석 정리

--- PopPool/PopPool/Presentation/Coordinator/AppCoordinator.swift
+++ PopPool/PopPool/Presentation/Coordinator/AppCoordinator.swift
@@ -14,25 +14,27 @@ class AppCoordinator: BaseCoordinator {
     override func start() {
         
         /// BaseCoordinator를 적용하지 않는 이유
-            // BaseCoordinator의 역할은 메서드들을 일일히 선언하지 않는 편의성을 위함입니다
-            // AppCoordinator의 start() 메서드 안에 BaseCoordinator를 담을 경우
-            // BaseCoordinator 자체는 값이 없기 때문에 화면이 보이지 않게 됩니다.
-            
-            // 또한 BaseCoordinator에는 '화면 전환' 메서드를 가지고 있지에
-            // baseCoordinator를 한번 더 감싼 MainCoordinator를 적용합니다
-              
+        // BaseCoordinator의 역할은 메서드들을 일일히 선언하지 않는 편의성을 위함입니다
+        // AppCoordinator의 start() 메서드 안에 BaseCoordinator를 담을 경우
+        // BaseCoordinator 자체는 값이 없기 때문에 화면이 보이지 않게 됩니다.
+        
+        // 또한 BaseCoordinator에는 '화면 전환' 메서드를 가지고 있지에
+        // baseCoordinator를 한번 더 감싼 MainCoordinator를 적용합니다
+        
         /// BaseCoordinator에 rootViewController를 선언하지 않는 이유
-            // 아래와 같이 코드를 직접 선언할 경우, root.coordinator 대리자 문제가 발생합니다
-            // AppCoordinator > MainCoordinator > BaseCoordinator 인 구조이기에
-            // 최상위 coordinator 역할을 App에게 위임할 경우,
-            // ViewController가 가지고 있는 coordinator 델리게이트 역할을 위임 시킬 수 있는 객체가 없습니다
-                  
-            // let rootViewModel = ViewControllerViewModel()
-            // let root = ViewController(viewModel: rootViewModel)
-            // root.coordinator = self
+        // 아래와 같이 코드를 직접 선언할 경우, root.coordinator 대리자 문제가 발생합니다
+        // AppCoordinator > MainCoordinator > BaseCoordinator 인 구조이기에
+        // 최상위 coordinator 역할을 App에게 위임할 경우,
+        // ViewController가 가지고 있는 coordinator 델리게이트 역할을 위임 시킬 수 있는 객체가 없습니다
+        
+        // let rootViewModel = ViewControllerViewModel()
+        // let root = ViewController(viewModel: rootViewModel)
+        // root.coordinator = self
         showHomeScreen()
     }
     
+    /// HomeCoordinator를 생성하여 navigationController를 주입하는 메서드입니다
+    /// 기존 방식보다 더 편하게 초기 화면을 띄워주기 위한 메서드입니다
     func showHomeScreen() {
         let coordinator = HomeCoordinator(navigationController: navigationController)
         coordinator.delegate = self
@@ -42,17 +44,32 @@ class AppCoordinator: BaseCoordinator {
 }
 
 extension AppCoordinator: HomeCoordinatorDelegate {
-        func pushToNextViewController() {
-            let coordinator = TestCoordinator(navigationController: navigationController)
-            coordinator.delegate = self
-            coordinator.start()
-            childCoordinator.append(coordinator)
-        }
+    
+    /// ViewController에서 지정된 다음 화면으로 push하는 메서드입니다
+    func pushToNextViewController() {
+        let coordinator = TestCoordinator(navigationController: navigationController)
+        coordinator.delegate = self
+        coordinator.start()
+        childCoordinator.append(coordinator)
+    }
 }
 
 extension AppCoordinator: TestCoordinatorDelegate {
+    
+    /// 이전 화면으로 되돌아갑니다
+    /// navigation stack에서 화면을 pop하는 방식입니다
     func popViewController() {
         navigationController.popViewController(animated: true)
         childCoordinator.removeLast()
+    }
+    
+    /// 지정한 Coordinator를 모달 방식으로 보여줍니다
+    /// 모달 방식이기에 navigationStack에 직접적으로 올라가지 않습니다
+    /// coordinator.presentStart() 참조
+    func presentViewController() {
+        let coordinator = HomeCoordinator(navigationController: navigationController)
+        coordinator.delegate = self
+        coordinator.presentStart()
+        childCoordinator.append(coordinator)
     }
 }
--- PopPool/PopPool/Presentation/Coordinator/Coordinator Test/TestCoordinator.swift
+++ PopPool/PopPool/Presentation/Coordinator/Coordinator Test/TestCoordinator.swift
@@ -7,22 +7,38 @@
 
 import UIKit
 
+/// 두번째 화면을 담당하는 Coordinator입니다
+/// pop와 present 방식을 실행할 수 있습니다
 protocol TestCoordinatorDelegate {
     func popViewController()
+    func presentViewController()
 }
 
-class TestCoordinator: BaseCoordinator, TestVCDelegate {
+class TestCoordinator: BaseCoordinator {
     
+    // MARK: - Properties
     var delegate: TestCoordinatorDelegate?
     
+    /// TestCoordinator 생성하는 메서드입니다
     override func start() {
         let viewModel = TestViewModel()
         let nextVC = TestVC(viewModel: viewModel)
         viewModel.delegate = self
         navigationController.pushViewController(nextVC, animated: true)
     }
+}
+
+extension TestCoordinator: TestVCDelegate {
     
+    // MARK: - Methods
+    
+    /// 화면을 navigationController stack에서 내립니다
     func popViewController() {
         delegate?.popViewController()
+    }
+    
+    /// 화면을 모달 방식으로 띄워줍니다
+    func presentViewController() {
+        delegate?.presentViewController()
     }
 }
