e75459d2020614e348ef2fdc846fe74081b83b53
JunYoung <ghddns34@gmail.com>
2024-06-18 22:51:32 +0900

    [FEAT] : ⚙️ identifier 기능 추가

--- PopPool/PopPool/Application/DIContainer/AppDIContainer.swift
+++ PopPool/PopPool/Application/DIContainer/AppDIContainer.swift
@@ -8,37 +8,70 @@
 import Foundation
 
 protocol DIContainer {
+    
     /// 특정 타입에 대한 컴포넌트를 등록합니다.
     /// - Parameters:
     ///   - type: 컴포넌트를 등록할 타입.
     ///   - component: 등록할 컴포넌트 인스턴스.
     func register<T>(type: T.Type, component: AnyObject)
     
+    /// 특정 타입에 대한 컴포넌트를 등록합니다.
+    /// - Parameters:
+    ///   - type: 컴포넌트를 등록할 타입.
+    ///   - identifier: 식별자
+    ///   - component: 등록할 컴포넌트 인스턴스.
+    func register<T>(type: T.Type, identifier: String, component: AnyObject)
+    
     /// 특정 타입에 대한 컴포넌트를 반환합니다.
     /// - Parameter type: 반환할 컴포넌트의 타입.
     /// - Returns: 반환된 컴포넌트 인스턴스.
     func resolve<T>(type: T.Type) -> T
+    
+    /// 특정 타입에 대한 컴포넌트를 반환합니다.
+    /// - Parameter type: 반환할 컴포넌트의 타입.
+    /// - identifier: 식별자
+    /// - Returns: 반환된 컴포넌트 인스턴스.
+    func resolve<T>(type: T.Type, identifier: String) -> T
 }
 
 final class AppDIContainer: DIContainer {
-    
+
     // AppDIContainer의 싱글톤 인스턴스
     static let shared = AppDIContainer()
     
     // 인스턴스 생성을 방지하기 위한 private 초기화 함수
     private init() {}
     
+    // Default 문자열
+    private var defaultString: String = "Default"
+    
     // 등록된 서비스를 저장할 딕셔너리
-    private var services: [String: AnyObject] = [:]
+    private var services: [String: [String : AnyObject] ] = [:]
     
     func register<T>(type: T.Type, component: AnyObject) {
         let key = "\(type)"
-        services[key] = component
+        services[key] = [defaultString : component]
+    }
+    
+    func register<T>(type: T.Type, identifier: String, component: AnyObject) {
+        let key = "\(type)"
+        if services[key] == nil {
+            services[key] = [identifier : component]
+        } else {
+            services[key]![identifier] = component
+        }
     }
     
     func resolve<T>(type: T.Type) -> T {
         let key = "\(type)"
-        return services[key] as! T
+        let subvalue = services[key]!
+        return subvalue[defaultString] as! T
+    }
+
+    func resolve<T>(type: T.Type, identifier: String) -> T {
+        let key = "\(type)"
+        let subvalue = services[key]!
+        return subvalue[identifier] as! T
     }
 }
 
