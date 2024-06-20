0a4313ae2d035f92b736f73fad8b7fdc0f0736a8
Madman-dev <dongjunlee222@gmail.com>
2024-06-20 18:32:55 +0900

    [FEAT]: ✨ Spacing Constant 생성

--- PopPool/PopPool/Presentation/Common/Utility/Constants.swift
+++ PopPool/PopPool/Presentation/Common/Utility/Constants.swift
@@ -7,3 +7,44 @@
 
 import Foundation
 
+// MARK: - Space Guide
+
+//struct SpaceGuide {
+//    let _4px = 4
+//    let _8px = 8
+//    let _12px = 12
+//    let _16px = 16
+//    let _20px = 20
+//    let _24px = 24
+//    let _28px = 28
+//    let _32px = 32
+//    let _36px = 36
+//    let _40px = 40
+//    let _48px = 48
+//    let _64px = 64
+//    let _80px = 80
+//    let _120px = 120
+//    let _156px = 156
+//}
+//
+//class Constants {
+//    static let spaceGuide: SpaceGuide = SpaceGuide()
+//}
+
+enum SpaceGuide {
+    static let _4px = 4
+    static let _8px = 8
+    static let _12px = 12
+    static let _16px = 16
+    static let _20px = 20
+    static let _24px = 24
+    static let _28px = 28
+    static let _32px = 32
+    static let _36px = 36
+    static let _40px = 40
+    static let _48px = 48
+    static let _64px = 64
+    static let _80px = 80
+    static let _120px = 120
+    static let _156px = 156
+}
