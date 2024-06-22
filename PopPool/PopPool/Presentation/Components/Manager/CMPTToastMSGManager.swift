c513a2faa931eb3b436455ba566b6e85c530f615
Madman-dev <dongjunlee222@gmail.com>
2024-06-23 01:08:54 +0900

    [FIX]: 🔧 Toast Message 싱글톤으로 변경, 초기 설정 구현

--- PopPool/PopPool/Presentation/Components/Manager/CMPTToastMSGManager.swift
+++ PopPool/PopPool/Presentation/Components/Manager/CMPTToastMSGManager.swift
@@ -1,5 +1,5 @@
 //
-//  CMPTToastMSGView.swift
+//  CMPTToastMSGManager.swift
 //  PopPool
 //
 //  Created by SeoJunYoung on 6/20/24.
@@ -12,51 +12,57 @@ import SnapKit
 class CMPTToastMSGManager {
     
     static let shared = CMPTToastMSGManager()
+    private init() {}
     
     private let messageLabel: UILabel = {
         let label = UILabel()
         label.textColor = .white
         label.textAlignment = .center
+        label.layer.cornerRadius = 4
+        label.clipsToBounds = true
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-        
-    private init() {
-        
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
 }
 
 extension CMPTToastMSGManager {
     
+    // 화면위에 올리고
+    // 새로 받는 정보로 업데이트
+    // 화면에서 사라져야함
     
+    private func setup(on superview: UIView) {
+        superview.addSubview(messageLabel)
+        
+        NSLayoutConstraint.activate([
+            // 화면 하단에 뜰 예정
+            // 화면 leading, trailing, bottom이 필요할 것
+            // 특정 길이를 제공하면... 텍스트 크기에 따라 바뀌는 값을 적용할 수 없을수도 있을테니 일단 기초 공사만
+            
+            messageLabel.centerXAnchor.constraint(equalTo: superview.centerXAnchor),
+            messageLabel.bottomAnchor.constraint(equalTo: superview.bottomAnchor, constant: -120),
+            messageLabel.widthAnchor.constraint(equalToConstant: 180),
+            messageLabel.heightAnchor.constraint(equalToConstant: 38)
+        ])
+    }
     
-    // 토스트 메시지가 화면에서 사라집니다
-//    func showMessage() {
-//        UIView.animate(
-//            withDuration: 4.0,
-//            delay: 0.1,
-//            options: .curveEaseOut
-//        ) { self.alpha = 0 } completion: { complete in
-//            self.removeFromSuperview()
-//            print("toastMessage가 화면에서 내려갔습니다")
-//        }
-//    }
-//
-//    private func setup() {
-//        // 기초 설정
-//        self.clipsToBounds = true
-//        self.layer.cornerRadius = 4
-//        self.backgroundColor = .gray
-//        
-//        // Layout 설정
-//        addSubview(messageLabel)
-//        messageLabel.snp.makeConstraints { make in
-//            make.height.equalTo(38)
-//            make.centerY.equalToSuperview()
-//            make.leading.trailing.equalToSuperview().inset(16)
-//        }
-//    }
+    private func updateMessage(message: String) {
+        messageLabel.text = message
+    }
+    
+    func createToastMSG(on superView: UIView, message: String) {
+        setup(on: superView)
+        updateMessage(message: message)
+        
+        DispatchQueue.main.async {
+            UIView.animate(
+                withDuration: 4.0,
+                delay: 3.0,
+                options: .curveEaseOut) {
+                    self.messageLabel.alpha = 0
+                } completion: { complete in
+                    self.messageLabel.removeFromSuperview()
+                }
+        }
+    }
 }
