ce1ec3d77a1b666e14633eb2cb06e9abe98f77c3
Madman-dev <dongjunlee222@gmail.com>
2024-06-24 00:20:00 +0900

    [HOTFIX]: 💥 영문, 국문 Font 코드 수정

--- PopPool/PopPool/Presentation/Common/Extension/UIFont.swift
+++ PopPool/PopPool/Presentation/Common/Extension/UIFont.swift
@@ -10,53 +10,19 @@ import UIKit
 
 extension UIFont {
     
-    /// 폰트의 고정된 값을 제공합니다
-    enum Size: CGFloat {
-        case _11 = 11
-        case _12 = 12
-        case _13 = 13
-        case _14 = 14
-        case _15 = 15
-        case _16 = 16
-        case _18 = 18
-        case _20 = 20
-        case _24 = 24
-        case _28 = 28
-        case _32 = 32
+    static func KorFont(style: FontStyle, size: CGFloat) -> UIFont? {
+        return UIFont(name: "GothicA1\(style.rawValue)", size: size)
+    }
+
+    static func EngFont(style: FontStyle, size: CGFloat) -> UIFont? {
+        return UIFont(name: "Poppins\(style.rawValue)", size: size)
     }
     
-    /// 영문 Poppins 폰트
-    enum EngFont {
-        case regular(size: Size)
-        case light(size: Size)
-        case medium(size: Size)
-        case bold(size: Size)
-        
-        var name: UIFont! {
-            switch self {
-            case .bold(let size): return UIFont(name: "Poppins-Bold", size: size.rawValue)
-            case .light(let size): return UIFont(name: "Poppins-Light", size: size.rawValue)
-            case .medium(let size): return UIFont(name: "Poppins-Medium", size: size.rawValue)
-            case .regular(let size): return UIFont(name: "Poppins-Regular", size: size.rawValue)
-            }
-        }
-    }
-    
-    /// 국문 Gothic 폰트
-    enum KorFont {
-        case regular(size: Size)
-        case light(size: Size)
-        case medium(size: Size)
-        case bold(size: Size)
-        
-        var name: UIFont! {
-            switch self {
-            case .bold(let size): return UIFont(name: "GothicA1-Bold", size: size.rawValue)
-            case .light(let size): return UIFont(name: "GothicA1-Light", size: size.rawValue)
-            case .medium(let size): return UIFont(name: "GothicA1-Medium", size: size.rawValue)
-            case .regular(let size): return UIFont(name: "GothicA1-Regular", size: size.rawValue)
-            }
-        }
+    enum FontStyle: String {
+        case bold = "-Bold"
+        case medium = "-Medium"
+        case regular = "-Regular"
+        case light = "-Light"
     }
 }
 
