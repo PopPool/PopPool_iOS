6ea2c53e032fb6de027522101d27ecbc7bfcbe74
Madman-dev <dongjunlee222@gmail.com>
2024-06-25 23:31:43 +0900

    [FIX]: ✨ InfoBox 컴포넌트 이메일 가리기 기능 추가

--- PopPool/PopPool/Presentation/Components/CMPTInfoBoxView.swift
+++ PopPool/PopPool/Presentation/Components/CMPTInfoBoxView.swift
@@ -11,28 +11,29 @@ import SnapKit
 
 class CMPTInfoBoxView: UIView {
     
-    let bgView: UIView = {
+    private let bgView: UIView = {
         let view = UIView()
-        view.backgroundColor = .g600
+        view.backgroundColor = .g50
         view.layer.cornerRadius = 4
         view.clipsToBounds = true
-//        view.translatesAutoresizingMaskIntoConstraints = false
+        view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
     
-    let logoImageView: UIImageView = {
+    private let logoImageView: UIImageView = {
         let imageView = UIImageView()
-        imageView.image = .brandAppleLight
-//        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = .infoLogoApple
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
     
-    let infoLabel: UILabel = {
+    private let infoLabel: UILabel = {
         let label = UILabel()
         label.font = .EngFont(style: .regular, size: 15)
         label.numberOfLines = 0
-        label.text = "이렇게 된다"
-//        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .g600
+        label.text = "abcdefg@gmail.com"
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
@@ -48,12 +49,26 @@ class CMPTInfoBoxView: UIView {
 
 extension CMPTInfoBoxView {
     
-    func updateLabel(info: String) {
-        calculateInfoLength()
+    func updateLabel(email: String) {
+        guard checkIfCorrectFormat(email: email) else { return }
+        let displayableEmail = hideInfo(email)
+        
+        DispatchQueue.main.async {
+            self.infoLabel.text = displayableEmail
+        }
     }
     
-    private func calculateInfoLength() {
+    private func hideInfo(_ sensitiveInfo: String) -> String {
+        let length = sensitiveInfo.count
+        if length <= 6 { return sensitiveInfo }
+        return String(sensitiveInfo.prefix(3) + String(repeating: "*", count: length - 4) + sensitiveInfo.suffix(4))
+    }
+    
+    private func checkIfCorrectFormat(email: String) -> Bool {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
         
+        return emailPredicate.evaluate(with: email)
     }
     
     private func setLayout() {
@@ -67,14 +82,13 @@ extension CMPTInfoBoxView {
         }
         
         logoImageView.snp.makeConstraints { make in
-            make.leading.equalToSuperview().inset(10)
+            make.leading.equalToSuperview().inset(41)
             make.centerY.equalToSuperview()
-            make.height.width.equalTo(20)
+            make.height.width.equalTo(22)
         }
         
         infoLabel.snp.makeConstraints { make in
-            make.leading.equalTo(logoImageView.snp.trailing).offset(10)
-            make.trailing.equalToSuperview().inset(10)
+            make.leading.equalTo(logoImageView.snp.trailing).offset(4)
             make.centerY.equalTo(logoImageView.snp.centerY)
         }
     }
