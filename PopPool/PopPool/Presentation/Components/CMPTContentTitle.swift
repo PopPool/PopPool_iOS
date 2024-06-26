adf6bbdc6a02be5f385244d670d5259693174e2f
JunYoung <112812473+dongglehada@users.noreply.github.com>
2024-06-26 14:16:04 +0900

    [MERGE]: ✅ CMPTContentTitle, CMPTCheckBox, CMPTTermsView, Sign Up Step View (#35)
    * [FEAT] : ⚙️ CMPTContentTitle
    * [FEAT] : ⚙️ 회원가입 Step 1

---
+++ PopPool/PopPool/Presentation/Components/CMPTContentTitle.swift
@@ -0,0 +1,224 @@
+//
+//  CMPTContentTitle.swift
+//  PopPool
+//
+//  Created by SeoJunYoung on 6/25/24.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+
+// 제목 타입 열거형 정의
+enum TYPEContentTitle {
+    case title_sub_fp(subTitle: String)
+    case title_fp
+    case title_icon_fp(icon: UIImage?)
+    case title_sub_bs(subTitle: String, buttonImage: UIImage?)
+    case title_bs(buttonImage: UIImage?)
+}
+
+// 제목 타입 확장
+extension TYPEContentTitle {
+    // 제목 폰트 설정
+    var titleFont: UIFont? {
+        switch self {
+        case .title_sub_fp:
+            return .KorFont(style: .bold, size: 20)
+        case .title_fp:
+            return .KorFont(style: .bold, size: 20)
+        case .title_icon_fp:
+            return .KorFont(style: .bold, size: 20)
+        case .title_sub_bs:
+            return .KorFont(style: .bold, size: 18)
+        case .title_bs:
+            return .KorFont(style: .bold, size: 18)
+        }
+    }
+    
+    // 부제 폰트 설정
+    var subFont: UIFont? {
+        switch self {
+        case .title_sub_fp(_):
+            return .KorFont(style: .regular, size: 15)
+        case .title_sub_bs(_, _):
+            return .KorFont(style: .regular, size: 15)
+        default:
+            return nil
+        }
+    }
+    
+    // 제목 색상 설정
+    var titleColor: UIColor? {
+        switch self {
+        default:
+            return .g1000
+        }
+    }
+    
+    // 부제 색상 설정
+    var subTitleColor: UIColor? {
+        switch self {
+        default:
+            return .g600
+        }
+    }
+}
+
+final class CMPTContentTitle: UIStackView {
+    
+    // MARK: - Components
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.lineBreakMode = . byWordWrapping
+        return label
+    }()
+    
+    private let subTitleLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    private let iconImageView: UIImageView = {
+        let view = UIImageView()
+        return view
+    }()
+    
+    let button: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+    
+    private let subStackView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .horizontal
+        view.alignment = .center
+        return view
+    }()
+    
+    init(title: String, type: TYPEContentTitle) {
+        super.init(frame: .zero)
+        setUp(title: title, type: type)
+        setUpViewType(type: type)
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension CMPTContentTitle {
+    
+    /// 기본 설정 메소드
+    /// - Parameters:
+    ///   - title: 제목
+    ///   - type: TYPEContentTitle
+    func setUp(title: String, type: TYPEContentTitle) {
+        self.axis = .vertical
+        self.spacing = 0
+        self.alignment = .fill
+        self.distribution = .fill
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineHeightMultiple = 1.5
+        titleLabel.font = type.titleFont
+        titleLabel.attributedText = NSMutableAttributedString(
+            string: title,
+            attributes: [NSAttributedString.Key.paragraphStyle: paragraphStyle]
+        )
+        
+        titleLabel.textColor = type.titleColor
+        subTitleLabel.font = type.subFont
+        subTitleLabel.textColor = type.subTitleColor
+    }
+    
+    /// 뷰 타입별 설정 메소드
+    /// - Parameter type: TYPEContentTitle
+    func setUpViewType(type: TYPEContentTitle) {
+        let topSpacingView = UIView()
+        let middleSpacingView = UIView()
+        
+        switch type {
+        case .title_sub_fp(let subTitle):
+            subTitleLabel.text = subTitle
+            
+            topSpacingView.snp.makeConstraints { make in
+                make.height.equalTo(SpaceGuide._64px)
+            }
+            middleSpacingView.snp.makeConstraints { make in
+                make.height.equalTo(SpaceGuide._16px)
+            }
+            
+            self.addArrangedSubview(topSpacingView)
+            self.addArrangedSubview(titleLabel)
+            self.addArrangedSubview(middleSpacingView)
+            self.addArrangedSubview(subTitleLabel)
+        case .title_fp:
+            topSpacingView.snp.makeConstraints { make in
+                make.height.equalTo(SpaceGuide._64px)
+            }
+            
+            self.addArrangedSubview(topSpacingView)
+            self.addArrangedSubview(titleLabel)
+        case .title_icon_fp(let icon):
+            iconImageView.image = icon
+            self.alignment = .leading
+            subStackView.spacing = 10
+            
+            topSpacingView.snp.makeConstraints { make in
+                make.height.equalTo(SpaceGuide._64px)
+            }
+
+            iconImageView.snp.makeConstraints { make in
+                make.size.equalTo(SpaceGuide._24px)
+            }
+            
+            subStackView.addArrangedSubview(titleLabel)
+            subStackView.addArrangedSubview(iconImageView)
+            self.addArrangedSubview(topSpacingView)
+            self.addArrangedSubview(subStackView)
+        case .title_sub_bs(let subTitle, let buttonImage):
+            subTitleLabel.text = subTitle
+            button.setImage(buttonImage, for: .normal)
+            
+            topSpacingView.snp.makeConstraints { make in
+                make.height.equalTo(SpaceGuide._32px)
+            }
+            middleSpacingView.snp.makeConstraints { make in
+                make.height.equalTo(SpaceGuide._8px)
+            }
+            button.snp.makeConstraints { make in
+                make.size.equalTo(24)
+            }
+            
+            subStackView.addArrangedSubview(titleLabel)
+            subStackView.addArrangedSubview(button)
+            self.addArrangedSubview(topSpacingView)
+            self.addArrangedSubview(subStackView)
+            self.addArrangedSubview(middleSpacingView)
+            self.addArrangedSubview(subTitleLabel)
+        case .title_bs(let buttonImage):
+            button.setImage(buttonImage, for: .normal)
+            
+            topSpacingView.snp.makeConstraints { make in
+                make.height.equalTo(SpaceGuide._32px)
+            }
+            button.snp.makeConstraints { make in
+                make.size.equalTo(24)
+            }
+            
+            subStackView.addArrangedSubview(titleLabel)
+            subStackView.addArrangedSubview(button)
+            self.addArrangedSubview(topSpacingView)
+            self.addArrangedSubview(subStackView)
+        }
+    }
+}
+
+extension CMPTContentTitle {
+    /// 제목 attributedText설정
+    /// - Parameter attributedText: NSAttributedString 값
+    func setTitleLabel(attributedText: NSAttributedString) {
+        titleLabel.attributedText = attributedText
+    }
+}
--- PopPool/PopPool/Presentation/Scene/OnBoarding/SignUp/VCSignUp.swift
+++ PopPool/PopPool/Presentation/Scene/OnBoarding/SignUp/VCSignUp.swift
@@ -14,45 +14,188 @@ import RxSwift
 final class VCSignUp: UIViewController {
     
     private let progressIndicator: CMPTProgressIndicator = CMPTProgressIndicator(totalStep: 4, startPoint: 1)
-    private let bottomButton: CMPTButton = CMPTButton(type: .kakao, title: "카카오톡으로 로그인")
-    private let midleButton: CMPTButton = CMPTButton(type: .apple, title: "Apple로 로그인")
-    private let disposeBag = DisposeBag()
     
+    // MARK: - ContentTitles
+    private var contentTitleViews: [CMPTContentTitle] = [
+        CMPTContentTitle(
+            title: "서비스 이용을 위한\n약관을 확인해주세요",
+            type: .title_fp
+        ),
+        CMPTContentTitle(
+            title: "팝풀에서 사용할\n별명을 설정해볼까요?",
+            type: .title_sub_fp(subTitle: "이 단계를 건너뛰시면 자동으로 별명이 만들어져요.")
+        ),
+        CMPTContentTitle(
+            title: "$유저명$님에 대해\n조금 더 알려주시겠어요?",
+            type: .title_fp
+        ),
+        CMPTContentTitle(
+            title: "$유저명$님에 대해\n조금 더 알려주시겠어요?",
+            type: .title_fp
+        )
+    ]
+    private let contentTitleStackView: UIStackView = UIStackView()
+    
+    // MARK: - ContentViews
+    private let step1View = SignUpStep1View()
+    lazy var contentViews = [
+        step1View,
+        UIView()
+    ]
+    private let contentStackView: UIStackView = UIStackView()
+    
+    // MARK: - Buttons
+    private let step1_primaryButton: CMPTButton = {
+        let button = CMPTButton(
+            type: .primary,
+            title: "확인",
+            disabledTitle: "확인"
+        )
+        button.isEnabled = false
+        return button
+    }()
+    private let step2_primaryButton = CMPTButton(type: .primary, title: "확인", disabledTitle: "다음")
+    private let step2_secondaryButton = CMPTButton(type: .secondary, title: "건너뛰기")
+    lazy var step2_buttons: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [self.step2_secondaryButton, self.step2_primaryButton])
+        view.spacing = 10
+        view.distribution = .fillEqually
+        return view
+    }()
+    private let step3_primaryButton = CMPTButton(type: .primary, title: "다음", disabledTitle: "다음")
+    private let step3_secondaryButton = CMPTButton(type: .secondary, title: "건너뛰기")
+    lazy var step3_buttons: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [self.step3_secondaryButton, self.step3_primaryButton])
+        view.spacing = 10
+        view.distribution = .fillEqually
+        return view
+    }()
+    private let step4_primaryButton = CMPTButton(type: .primary, title: "다음", disabledTitle: "다음")
+    private let step4_secondaryButton = CMPTButton(type: .secondary, title: "건너뛰기")
+    lazy var step4_buttons: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [self.step4_secondaryButton, self.step4_primaryButton])
+        view.spacing = 10
+        view.distribution = .fillEqually
+        return view
+    }()
+    lazy var bottomButtons = [
+        step1_primaryButton,
+        step2_buttons,
+        step3_buttons,
+        step4_buttons
+        
+    ]
+    private let buttonStackView: UIStackView = UIStackView()
+    
+    // MARK: - Properties
+    private let viewModel = VMSignUp()
+    private let disposeBag = DisposeBag()
+}
+
+// MARK: - Life Cycle
+extension VCSignUp {
     override func viewDidLoad() {
         view.backgroundColor = .systemBackground
         setUpConstraints()
+        bind()
     }
+}
+// MARK: - SetUp
+private extension VCSignUp {
     
+    /// UI 요소의 레이아웃 제약 설정
     func setUpConstraints() {
+        contentTitleStackView.addArrangedSubview(contentTitleViews[0])
+        contentStackView.addArrangedSubview(contentViews[0])
+        buttonStackView.addArrangedSubview(bottomButtons[0])
         view.addSubview(progressIndicator)
-        view.addSubview(midleButton)
-        view.addSubview(bottomButton)
+        view.addSubview(contentTitleStackView)
+        view.addSubview(contentStackView)
+        view.addSubview(buttonStackView)
         
         progressIndicator.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide).inset(SpaceGuide._16px)
-            make.leading.trailing.equalToSuperview().inset(20)
+            make.leading.trailing.equalToSuperview().inset(SpaceGuide._20px)
         }
-        
-        midleButton.snp.makeConstraints { make in
-            make.leading.trailing.equalToSuperview().inset(20)
-            make.height.equalTo(52)
-            make.center.equalToSuperview()
+        contentTitleStackView.snp.makeConstraints { make in
+            make.top.equalTo(progressIndicator.snp.bottom)
+            make.leading.trailing.equalToSuperview().inset(SpaceGuide._20px)
         }
-        
-        bottomButton.snp.makeConstraints { make in
-            make.leading.trailing.equalToSuperview().inset(20)
+        contentStackView.snp.makeConstraints { make in
+            make.top.equalTo(contentTitleStackView.snp.bottom)
+            make.leading.trailing.equalToSuperview().inset(SpaceGuide._20px)
+        }
+        buttonStackView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(SpaceGuide._20px)
             make.height.equalTo(52)
             make.bottom.equalToSuperview().inset(SpaceGuide._48px)
         }
+    }
+    
+    /// ViewModel과의 바인딩을 설정
+    func bind() {
+        let input = VMSignUp.Input(
+            tap_step1_primaryButton: step1_primaryButton.rx.tap,
+            tap_step2_primaryButton: step2_primaryButton.rx.tap,
+            tap_step3_primaryButton: step3_primaryButton.rx.tap,
+            didChagneTerms: step1View.terms
+        )
+        let output = viewModel.transform(input: input)
         
-        bottomButton.rx.tap.subscribe { _ in
-            self.progressIndicator.increaseIndicator()
-        }
-        .disposed(by: disposeBag)
+        // 페이지 인덱스 증가 이벤트 처리
+        output.increasePageIndex
+            .withUnretained(self)
+            .subscribe { (owner, pageIndex) in
+                owner.progressIndicator.increaseIndicator()
+                owner.changeViewFrom(pageIndex: pageIndex, previousIndex: pageIndex - 1)
+            }
+            .disposed(by: disposeBag)
         
-        midleButton.rx.tap.subscribe { _ in
-            self.progressIndicator.decreaseIndicator()
+        // Step 1 primary button 활성/비활성 상태 처리
+        output.step1_primaryButton_isEnabled
+            .withUnretained(self)
+            .subscribe { (owner, isEnabled) in
+                owner.changeButtonState(button: owner.step1_primaryButton, isEnabled: isEnabled)
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    /// 페이지 인덱스에 따라 뷰 변경을 처리하는 메서드
+    ///
+    /// - Parameters:
+    ///   - pageIndex: 현재 페이지 인덱스
+    ///   - previousIndex: 이전 페이지 인덱스
+    func changeViewFrom(pageIndex: Int, previousIndex: Int) {
+        if pageIndex < contentTitleViews.count {
+            UIView.transition(with: contentTitleStackView, duration: 0.2, options: .transitionFlipFromBottom) {
+                self.contentTitleViews[previousIndex].removeFromSuperview()
+                self.contentTitleStackView.addArrangedSubview(self.contentTitleViews[pageIndex])
+            }
         }
-        .disposed(by: disposeBag)
+        if pageIndex < contentViews.count {
+            UIView.transition(with: contentStackView, duration: 0.2, options: .transitionFlipFromBottom) {
+                self.contentViews[previousIndex].removeFromSuperview()
+                self.contentStackView.addArrangedSubview(self.contentViews[pageIndex])
+            }
+        }
+        if pageIndex < bottomButtons.count {
+            UIView.transition(with: buttonStackView, duration: 0.2, options: .transitionFlipFromBottom) {
+                self.bottomButtons[previousIndex].removeFromSuperview()
+                self.buttonStackView.addArrangedSubview(self.bottomButtons[pageIndex])
+            }
+        }
+    }
+    
+    /// 버튼의 활성/비활성 상태를 변경하는 메서드
+    ///
+    /// - Parameters:
+    ///   - button: 대상 버튼
+    ///   - isEnabled: 활성화 여부
+    func changeButtonState(button: UIButton, isEnabled: Bool) {
+        if button.isEnabled != isEnabled {
+            UIView.transition(with: button, duration: 0.2, options: .transitionCrossDissolve) {
+                button.isEnabled = isEnabled
+            }
+        }
     }
 }
