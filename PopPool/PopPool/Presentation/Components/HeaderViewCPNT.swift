d21ba75ee0fff17db036ae3b1f5c002682a7ce0e
Madman-dev <dongjunlee222@gmail.com>
2024-06-27 15:23:54 +0900

    [DOCS]: 📚 HeaderView 주석 정리

--- PopPool/PopPool/Presentation/Components/HeaderViewCPNT.swift
+++ PopPool/PopPool/Presentation/Components/HeaderViewCPNT.swift
@@ -9,14 +9,16 @@ import Foundation
 import UIKit
 import SnapKit
 
-class HeaderViewCPNT: UIStackView {
+final class HeaderViewCPNT: UIStackView {
     
     enum Style {
         case icon
         case text
     }
     
-    let backButton: UIButton = {
+    // MARK: - Properties
+    
+    let leftBarButton: UIButton = {
         let button = UIButton(type: .system)
         button.tintColor = .g1000
         button.setContentHuggingPriority(.required, for: .horizontal)
@@ -31,12 +33,14 @@ class HeaderViewCPNT: UIStackView {
         return label
     }()
     
-    let iconButton: UIButton = {
+    let rightBarButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitleColor(.g1000, for: .normal)
         button.setContentHuggingPriority(.required, for: .horizontal)
         return button
     }()
+    
+    // MARK: - Initializer
     
     init(title: String, style: Style) {
         super.init(frame: .zero)
@@ -47,7 +51,11 @@ class HeaderViewCPNT: UIStackView {
     required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
+
+extension HeaderViewCPNT {
     
+    /// 기본 헤더 뷰의 화면 구성
     private func setupLayout() {
         self.axis = .horizontal
         self.distribution = .fillProportionally
@@ -56,22 +64,27 @@ class HeaderViewCPNT: UIStackView {
         self.isLayoutMarginsRelativeArrangement = true
         self.layoutMargins = UIEdgeInsets(top: 0, left: 12, bottom: 0, right: 16)
         
-        self.addArrangedSubview(backButton)
+        self.addArrangedSubview(leftBarButton)
         self.addArrangedSubview(titleLabel)
-        self.addArrangedSubview(iconButton)
+        self.addArrangedSubview(rightBarButton)
     }
     
+    /// 헤더 뷰의 컴포넌트 설정
+    /// - Parameters:
+    ///   - title: 바꿀 제목명을 받습니다
+    ///   - style: rightBarButton의 아이콘 타입 (ie. text / icon)
     private func setupViews(title: String, style: Style) {
         
-        titleLabel.text = title
         switch style {
         case .icon:
-            backButton.setImage(.icoLine, for: .normal)
-            iconButton.setImage(.icoSolid, for: .normal)
+            leftBarButton.setImage(.icoLine, for: .normal)
+            rightBarButton.setImage(.icoSolid, for: .normal)
         case .text:
-            backButton.setImage(.icoLine, for: .normal)
-            iconButton.setTitle("둘러보기", for: .normal)
-            iconButton.titleLabel?.font = .KorFont(style: .regular, size: 14)
+            leftBarButton.setImage(.icoLine, for: .normal)
+            rightBarButton.setTitle("둘러보기", for: .normal)
+            rightBarButton.titleLabel?.font = .KorFont(style: .regular, size: 14)
         }
+        titleLabel.text = title
     }
+
 }
