098360eca5704c7d9133902f78a40787b8e8a2e8
Madman-dev <dongjunlee222@gmail.com>
2024-06-28 03:48:37 +0900

    [FEAT]: ✨ 간편히 간격을 생성하기 위해 spacerFactory 구현

--- PopPool/PopPool/Presentation/Common/Utility/Factory.swift
+++ PopPool/PopPool/Presentation/Common/Utility/Factory.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import UIKit
+import SnapKit
+
+class SpacingFactory {
+    static let shared = SpacingFactory()
+    private init() {}
+    
+    func createSpace(on superview: UIView, size: Int) -> UIView {
+        let spacer = UIView()
+        superview.addSubview(spacer)
+        
+        spacer.snp.makeConstraints { make in
+            make.height.equalTo(size)
+        }
+        return spacer
+    }
+}
