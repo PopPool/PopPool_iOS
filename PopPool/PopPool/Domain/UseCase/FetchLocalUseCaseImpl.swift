43c200818b3bba71d1685d5a7062f632e5e6b019
Madman-dev <dongjunlee222@gmail.com>
2024-07-15 00:37:34 +0900

    [REMOVE]: ✂️ LocalDBUseCase 파일 분리 및 삭제

--- PopPool/PopPool/Domain/UseCase/FetchLocalUseCaseImpl.swift
+++ PopPool/PopPool/Domain/UseCase/FetchLocalUseCaseImpl.swift
@@ -8,6 +8,17 @@
 import Foundation
 import RxSwift
 
+protocol FetchLocalUseCase {
+    var repository: LocalDBRepository { get set }
+    
+    /// 로컬 DB에서 값을 가져옵니다
+    /// - Parameters:
+    ///   - key: 계정 이름 ie) ID, accessKey 등
+    ///   - from: 로컬 DB
+    /// - Returns: 로컬 DB에서 지정된 키의 값을 반환합니다`_40px`
+    func execute(key: String) -> Single<String>
+}
+
 final class FetchLocalUseCaseImpl: FetchLocalUseCase {
     
     var repository: LocalDBRepository
--- PopPool/PopPool/Domain/UseCase/SaveLocalUseCaseImpl.swift
+++ PopPool/PopPool/Domain/UseCase/SaveLocalUseCaseImpl.swift
@@ -8,6 +8,18 @@
 import Foundation
 import RxSwift
 
+protocol SaveLocalUseCase {
+    var repository: LocalDBRepository { get set }
+    
+    /// 로컬 DB에 값을 저장합니다.
+    /// - Parameters:
+    ///   - key: 계정 이름 ie) ID, accessKey 등
+    ///   - value: DB에 저장하는 데이터
+    ///   - to: 로컬 DB
+    /// - Returns: 반환하는 값은 없음
+    func execute(key: String, value: String) -> Completable
+}
+
 final class SaveLocalUseCaseImpl: SaveLocalUseCase {
 
     var repository: LocalDBRepository
