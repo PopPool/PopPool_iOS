fb76dda6a48d096adc9770a4c4c4f4601616d4e4
JunYoung <ghddns34@gmail.com>
2024-07-19 13:56:45 +0900

    [FEAT] : ⚙️ 관심사 선택 컬렉션뷰 모듈화

--- PopPool/PopPool/Data/Repositories/SignUpRepository/SignUpRepositoryTest.swift
+++ PopPool/PopPool/Data/Repositories/SignUpRepository/SignUpRepositoryTest.swift
@@ -11,18 +11,24 @@ import RxSwift
 final class SignUpRepositoryTest: SignUpRepository {
     
     func checkNickName(nickName: String) -> Observable<Bool> {
-        return Observable.just(true)
+        return Observable.just(false)
     }
     
     func fetchInterestList() -> Observable<[Interest]> {
         
         let interest:[Interest] = [
-            .init(interestID: 0, interestName: "패션"),
-            .init(interestID: 1, interestName: "패션1"),
-            .init(interestID: 2, interestName: "패션2"),
-            .init(interestID: 3, interestName: "패션3"),
-            .init(interestID: 4, interestName: "패션4"),
-            .init(interestID: 5, interestName: "패션5")
+            .init(interestID: 0, interestName: "게임"),
+            .init(interestID: 1, interestName: "라이프스타일"),
+            .init(interestID: 2, interestName: "반려동물"),
+            .init(interestID: 3, interestName: "뷰티"),
+            .init(interestID: 4, interestName: "스포츠"),
+            .init(interestID: 5, interestName: "애니메이션"),
+            .init(interestID: 6, interestName: "엔터테인먼트"),
+            .init(interestID: 7, interestName: "여행"),
+            .init(interestID: 8, interestName: "예술"),
+            .init(interestID: 9, interestName: "음식/요리"),
+            .init(interestID: 10, interestName: "키즈"),
+            .init(interestID: 11, interestName: "패션")
         ]
         return Observable.just(interest)
     }
---
+++ PopPool/PopPool/Presentation/Shared/InterestSelectedView.swift
@@ -0,0 +1,111 @@
+//
+//  InterestSelectedView.swift
+//  PopPool
+//
+//  Created by SeoJunYoung on 7/19/24.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+
+final class InterestSelectedView: UIView {
+    
+    // MARK: - Components
+    private let categoryCollectionView: UICollectionView = {
+        let layout = TagsLayout()
+        layout.minimumLineSpacing = 16
+        layout.minimumInteritemSpacing = 12
+        layout.scrollDirection = .vertical
+        let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        view.register(LargeChipCell.self, forCellWithReuseIdentifier: LargeChipCell.identifier)
+        view.allowsMultipleSelection = true
+        return view
+    }()
+    
+    // MARK: - Properties
+    private let disposeBag = DisposeBag()
+    
+    let selectedList: BehaviorRelay<[IndexPath]> = .init(value: [])
+    
+    let categoryList: BehaviorRelay<[String]> = .init(value: [])
+    
+    // MARK: - init
+    init() {
+        super.init(frame: .zero)
+        setUp()
+        bind()
+        setUpConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - SetUp
+private extension InterestSelectedView {
+    func setUp() {
+        categoryCollectionView.delegate = self
+        categoryCollectionView.dataSource = self
+    }
+    
+    func setUpConstraints() {
+        self.addSubview(categoryCollectionView)
+        categoryCollectionView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    /// 데이터 바인딩 메서드
+    func bind() {
+        categoryList
+            .withUnretained(self)
+            .subscribe { (owner, _) in
+                owner.categoryCollectionView.reloadData()
+            }
+            .disposed(by: disposeBag)
+    }
+}
+
+extension InterestSelectedView: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return categoryList.value.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: LargeChipCell.identifier, for: indexPath) as? LargeChipCell else {
+            return UICollectionViewCell()
+        }
+        cell.injectionWith(input: .init(title: categoryList.value[indexPath.row]))
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cell = LargeChipCell()
+        let title = categoryList.value[indexPath.row]
+        cell.injectionWith(input: .init(title: title))
+        return cell.adjustCellSize(title: title)
+    }
+    
+    /// 셀이 선택되었을 때의 동작 정의
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let limitSelectedCount = 5
+        let selectedListValue = selectedList.value
+        if selectedListValue.count >= limitSelectedCount {
+            collectionView.deselectItem(at: indexPath, animated: false)
+            ToastMSGManager.createToast(message: "최대 5개 까지 선택할 수 있어요")
+        } else {
+            selectedList.accept(selectedListValue + [indexPath])
+        }
+    }
+    
+    /// 셀이 선택 해제되었을 때의 동작 정의
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        var selectedListValue = selectedList.value
+        let targetIndex = selectedListValue.firstIndex(of: indexPath)!
+        selectedListValue.remove(at: targetIndex)
+        selectedList.accept(selectedListValue)
+    }
+}
