1bcfba789907f77af1490817738bc4cf92ec24b0
JunYoung <112812473+dongglehada@users.noreply.github.com>
2024-08-16 17:44:58 +0900

    [MERGE]: ✅ Profile Update 기능연결, DTO 수정 (#83)
    * [FEAT] : ⚙️ ProfileResponse DTO 수정, EditPage 연결, EditPage 네트워크 일부 연결
    
    * [FEAT] : ⚙️ ProfileUpdate 기능 연결, DTO 수정
    
    ---------
    
    Co-authored-by: Jack Lee <dongjunlee222@gmail.com>

--- PopPool/PopPool/Application/SceneDelegate.swift
+++ PopPool/PopPool/Application/SceneDelegate.swift
@@ -5,13 +5,6 @@
 //  Created by SeoJunYoung on 6/1/24.
 //
 
-//
-//  SceneDelegate.swift
-//  PopPool
-//
-//  Created by SeoJunYoung on 6/1/24.
-//
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
@@ -21,10 +14,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-
+        
         let navigationController = UINavigationController()
-
-        // 기존 코드 (주석 처리)
+        
 //        window?.rootViewController = UINavigationController(rootViewController: ViewController(viewModel: ViewControllerViewModel()))
         window?.rootViewController = UINavigationController(rootViewController: LoginVC(viewModel: LoginVM()))
         //window?.rootViewController = UINavigationController(rootViewController: BlockedUserVC(blockCount: 3))
@@ -32,7 +24,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
-    }
+    } 
 
     func sceneDidBecomeActive(_ scene: UIScene) {
     }
@@ -45,4 +37,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func sceneDidEnterBackground(_ scene: UIScene) {
     }
+
+
 }
+
