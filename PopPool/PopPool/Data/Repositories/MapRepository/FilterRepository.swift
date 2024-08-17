c2442438df9100a3201867ebd6927b3ae7395175
Madman-dev <dongjunlee222@gmail.com>
2024-08-17 14:11:27 +0900

    [HotFix]: 💥🚚 FilterUsecase 및 implementation 주석 처리

--- PopPool/PopPool/Data/Repositories/MapRepository/FilterRepository.swift
+++ PopPool/PopPool/Data/Repositories/MapRepository/FilterRepository.swift
@@ -8,27 +8,27 @@
 import RxSwift
 
 
-class FilterRepository {
-    private let remoteDataSource: RemoteDataSource
-
-    init(remoteDataSource: RemoteDataSource) {
-        self.remoteDataSource = remoteDataSource
-    }
-
-    func addFilter(_ filter: Filter) -> Single<[Filter]> {
-        return remoteDataSource.addFilter(filter)
-    }
-
-    func removeFilter(_ filter: Filter) -> Single<[Filter]> {
-        return remoteDataSource.removeFilter(filter)
-    }
-
-    func resetFilters() -> Single<Void> {
-        return remoteDataSource.resetFilters()
-    }
-
-    func applyFilters(_ filters: [Filter]) -> Completable {
-        return remoteDataSource.applyFilters(filters)
-    }
-}
-
+//class FilterRepository {
+//    private let remoteDataSource: RemoteDataSource
+//
+//    init(remoteDataSource: RemoteDataSource) {
+//        self.remoteDataSource = remoteDataSource
+//    }
+//
+//    func addFilter(_ filter: Filter) -> Single<[Filter]> {
+//        return remoteDataSource.addFilter(filter)
+//    }
+//
+//    func removeFilter(_ filter: Filter) -> Single<[Filter]> {
+//        return remoteDataSource.removeFilter(filter)
+//    }
+//
+//    func resetFilters() -> Single<Void> {
+//        return remoteDataSource.resetFilters()
+//    }
+//
+//    func applyFilters(_ filters: [Filter]) -> Completable {
+//        return remoteDataSource.applyFilters(filters)
+//    }
+//}
+//
--- PopPool/PopPool/Presentation/Scene/Map/PopUpListView/FilterBottomSheetViewModel.swift
+++ PopPool/PopPool/Presentation/Scene/Map/PopUpListView/FilterBottomSheetViewModel.swift
@@ -8,47 +8,47 @@
 import RxSwift
 import RxCocoa
 
-class FilterBottomSheetViewModel {
-    private let filterUseCase: FilterUseCase
-    private let disposeBag = DisposeBag()
-
-    var selectedFilters = BehaviorRelay<[Filter]>(value: [])
-    var locationData = BehaviorRelay<[Location]>(value: [])
-    var categories = BehaviorRelay<[Category]>(value: [])
-
-    init(filterUseCase: FilterUseCase) {
-        self.filterUseCase = filterUseCase
-    }
-
-    func addFilter(_ filter: Filter) {
-        filterUseCase.addFilter(filter)
-            .subscribe(onSuccess: { [weak self] filters in
-                self?.selectedFilters.accept(filters)
-            })
-            .disposed(by: disposeBag)
-    }
-
-    func removeFilter(_ filter: Filter) {
-        filterUseCase.removeFilter(filter)
-            .subscribe(onSuccess: { [weak self] filters in
-                self?.selectedFilters.accept(filters)
-            })
-            .disposed(by: disposeBag)
-    }
-
-    func resetFilters() {
-        filterUseCase.resetFilters()
-            .subscribe(onSuccess: { [weak self] _ in
-                self?.selectedFilters.accept([])
-            })
-            .disposed(by: disposeBag)
-    }
-
-    func applyFilters() {
-        filterUseCase.applyFilters(selectedFilters.value)
-            .subscribe(onCompleted: {
-                // 완료 시 동작
-            })
-            .disposed(by: disposeBag)
-    }
-}
+//class FilterBottomSheetViewModel {
+//    private let filterUseCase: FilterUseCase
+//    private let disposeBag = DisposeBag()
+//
+//    var selectedFilters = BehaviorRelay<[Filter]>(value: [])
+//    var locationData = BehaviorRelay<[Location]>(value: [])
+//    var categories = BehaviorRelay<[Category]>(value: [])
+//
+//    init(filterUseCase: FilterUseCase) {
+//        self.filterUseCase = filterUseCase
+//    }
+//
+//    func addFilter(_ filter: Filter) {
+//        filterUseCase.addFilter(filter)
+//            .subscribe(onSuccess: { [weak self] filters in
+//                self?.selectedFilters.accept(filters)
+//            })
+//            .disposed(by: disposeBag)
+//    }
+//
+//    func removeFilter(_ filter: Filter) {
+//        filterUseCase.removeFilter(filter)
+//            .subscribe(onSuccess: { [weak self] filters in
+//                self?.selectedFilters.accept(filters)
+//            })
+//            .disposed(by: disposeBag)
+//    }
+//
+//    func resetFilters() {
+//        filterUseCase.resetFilters()
+//            .subscribe(onSuccess: { [weak self] _ in
+//                self?.selectedFilters.accept([])
+//            })
+//            .disposed(by: disposeBag)
+//    }
+//
+//    func applyFilters() {
+//        filterUseCase.applyFilters(selectedFilters.value)
+//            .subscribe(onCompleted: {
+//                // 완료 시 동작
+//            })
+//            .disposed(by: disposeBag)
+//    }
+//}
