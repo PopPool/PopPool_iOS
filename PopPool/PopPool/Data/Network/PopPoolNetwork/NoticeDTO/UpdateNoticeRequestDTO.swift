082db1013c70ffc1dc19f98a0a8cecd1d5f8f023
JunYoung <ghddns34@gmail.com>
2024-08-18 21:29:11 +0900

    [FEAT] : Notice API EndPoint, DTO 추가

---
+++ PopPool/PopPool/Data/Network/PopPoolNetwork/NoticeDTO/UpdateNoticeRequestDTO.swift
@@ -0,0 +1,13 @@
+//
+//  UpdateNoticeRequestDTO.swift
+//  PopPool
+//
+//  Created by SeoJunYoung on 8/18/24.
+//
+
+import Foundation
+
+struct UpdateNoticeRequestDTO: Encodable {
+    var title: String
+    var content: String
+}
