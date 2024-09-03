ae16ee87a94265cb7062e4e3bfa8f401d28eca4e
Madman-dev <dongjunlee222@gmail.com>
2024-09-03 15:23:54 +0900

    FIX: 🔧️ 시점에 따른 알람 설정 토글

--- PopPool/PopPool/Presentation/Scene/Home/AlarmSettingVC.swift
+++ PopPool/PopPool/Presentation/Scene/Home/AlarmSettingVC.swift
@@ -8,7 +8,7 @@
 import UIKit
 import SnapKit
 import RxSwift
-import RxRelay
+import RxCocoa
 
 final class AlarmSettingVC: BaseViewController {
     
@@ -61,53 +61,80 @@ final class AlarmSettingVC: BaseViewController {
     }
     
     // MARK: - Methods
-    
     private func bind() {
-        headerView.leftBarButton.rx.tap
-            .subscribe(onNext: { [weak self] _ in
-                self?.navigationController?.popViewController(animated: true)
+        NotificationCenter.default.rx.notification(
+            UIApplication.didBecomeActiveNotification)
+            .withUnretained(self)
+            .subscribe(onNext: { (owner, _) in
+                owner.didBecomeActive()
+            })
+        
+        let input = AlarmSettingVM.Input(
+            isAlarmToggled: appPush.actionToggle.rx.isOn,
+            isActivityToggled: activityPush.actionToggle.rx.isOn,
+            returnTapped: headerView.leftBarButton.rx.tap
+        )
+        let output = viewModel.transform(input: input)
+        
+        output.returnTapped
+            .withUnretained(self)
+            .subscribe(onNext: { (owner, _) in
+                owner.navigationController?.popViewController(animated: true)
             })
             .disposed(by: disposeBag)
         
-        appPush.actionToggle.rx.isOn
-            .subscribe(onNext: { [weak self] isOn in
-                self?.viewModel.checkSetting(isOn: isOn)
-                    .subscribe(onNext: { [weak self] isAuthorized in
-                        print("허가 여부", isAuthorized)
-                        if !isAuthorized {
-                            self?.createSettingAlert(isOn: isOn)
-                        }
-                    })
-                    .disposed(by: self?.disposeBag ?? DisposeBag())
-            })
-            .disposed(by: disposeBag)
-        
-        activityPush.actionToggle.rx.isOn
-            .subscribe(onNext: { [weak self] isOn in
-                self?.createSettingAlert(isOn: isOn)
+        output.displayAlert
+            .withUnretained(self)
+            .subscribe(onNext: { (owner, status) in
+                let (isOn, isAuthorized) = status
+                if isOn && !isAuthorized {
+                    owner.createSettingAlert()
+                }
             })
             .disposed(by: disposeBag)
     }
     
-    private func createSettingAlert(isOn: Bool) {
+    /// 사용자가 설정 페이지의 알람 설정을 껐는지 확인, 상태에 맞춰 switch를 변경하는 메서드
+    private func didBecomeActive() {
+        viewModel.checkNotificationSetting()
+            .withUnretained(self)
+            .subscribe(onNext: { (owner, permission) in
+                DispatchQueue.main.async {
+                    if owner.appPush.actionToggle.isOn && !permission {
+                        owner.appPush.actionToggle.setOn(false, animated: true)
+                    }
+                }
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func createSettingAlert() {
         DispatchQueue.main.async { [weak self] in
             let alert = UIAlertController(
                 title: "알림 설정",
-                message: """
-            기기의 알림 설정이 껴져있습니다.\n휴대푠 설정 > 알림 > 팝풀에서\n설정을 변경해주세요.
-            """,
+                message: "기기의 알림 설정이 껴져있습니다.\n휴대푠 설정 > 알림 > 팝풀에서\n설정을 변경해주세요.",
                 preferredStyle: .alert)
             
-            alert.addAction(UIAlertAction(title: "취소", style: .cancel, handler: { [weak self] _ in
-                self?.appPush.actionToggle.setOn(!isOn, animated: true)
-            }))
+            alert.addAction(
+                UIAlertAction(title: "취소",
+                              style: .cancel,
+                              handler: { _ in
+                                  if let isOn = self?.appPush.actionToggle.isOn {
+                                      self?.appPush.actionToggle
+                                          .setOn(!isOn, animated: true)
+                                  }
+                              }))
             
-            alert.addAction(UIAlertAction(title: "설정", style: .default, handler: { _ in
-                let url = URL(string: UIApplication.openSettingsURLString)
-                if isOn {
-                    UIApplication.shared.open(url!)
-                }
-            }))
+            alert.addAction(
+                UIAlertAction(title: "설정",
+                              style: .default,
+                              handler: { _ in
+                                  let url = URL(string: UIApplication.openSettingsURLString)
+                                  if let url = url {
+                                      // 디바이스 설정 화면으로 이동
+                                      UIApplication.shared.open(url)
+                                  }
+                              }))
             
             self?.present(alert, animated: true)
         }
--- PopPool/PopPool/Presentation/Scene/Home/AlarmSettingVM.swift
+++ PopPool/PopPool/Presentation/Scene/Home/AlarmSettingVM.swift
@@ -10,36 +10,111 @@ import RxSwift
 import RxCocoa
 import UserNotifications
 
-class AlarmSettingVM: ViewModelable {
+final class AlarmSettingVM: ViewModelable {
     struct Input {
-        var isAlarmToggled: ControlEvent<Bool>
+        let isAlarmToggled: ControlProperty<Bool>
+        let isActivityToggled: ControlProperty<Bool>
+        let returnTapped: ControlEvent<Void>
     }
     
     struct Output {
-        
+        let returnTapped: Observable<Void>
+        let displayAlert: BehaviorRelay<(Bool, Bool)>
     }
     
+    private let center = UNUserNotificationCenter.current()
+    private let options: UNAuthorizationOptions = [.alert, .badge, .sound]
+    
+    private var isPermissionOn = BehaviorRelay<(Bool, Bool)>(value: (false, false))
     var disposeBag = DisposeBag()
     
-    func checkSetting(isOn: Bool) -> Observable<Bool> {
+    func checkNotificationSetting() -> Observable<Bool> {
         return Observable.create { observer in
-            let center = UNUserNotificationCenter.current()
-            
-            center.getNotificationSettings { setting in
-                if setting.authorizationStatus != .authorized {
-                    observer.onNext(false)
-                    observer.onCompleted()
-                } else if setting.authorizationStatus == .authorized {
-                    print("화면 설정 실패")
+            self.center.getNotificationSettings { permission in
+                switch permission.authorizationStatus {
+                case .authorized:
                     observer.onNext(true)
-                    observer.onCompleted()
+                default:
+                    observer.onNext(false)
                 }
+                observer.onCompleted()
             }
             return Disposables.create()
         }
     }
     
+    private func requestNotification() -> Observable<Bool> {
+        return Observable.create { observer in
+            self.center.requestAuthorization(options: self.options) { granted, error in
+                observer.onNext(granted)
+                observer.onCompleted()
+            }
+            return Disposables.create()
+        }
+    }
+    
+    
     func transform(input: Input) -> Output {
-        return Output()
+        input.isAlarmToggled
+            .withUnretained(self)
+            .subscribe(onNext: { (owner, isOn) in
+                print("===버튼 토글 O")
+                owner.checkNotificationSetting()
+                    .flatMapLatest { isAuthorized in
+                        if isOn && !isAuthorized {
+                            owner.isPermissionOn.accept((isOn, isAuthorized))
+                            return owner.requestNotification()
+                        } else {
+                            owner.isPermissionOn.accept((isOn, isAuthorized))
+                            return .just(isAuthorized)
+                        }
+                    }
+                    .subscribe(onNext: { isAuthorized in
+                        // 여기서 현재 설정 상태 트래킹?
+                        print("상태 트래킹", isAuthorized)
+                        owner.isPermissionOn.accept((isOn, isAuthorized))
+                    })
+                    .disposed(by: owner.disposeBag)
+            })
+            .disposed(by: disposeBag)
+        
+//        let alarmStatus = input.isAlarmToggled
+//            .flatMapLatest { isOn in
+//                if isOn {
+//                    print("====버튼 토글 O")
+//                    return self.checkNotificationSetting()
+//                        .flatMapLatest { isAuthorized -> Observable<(Bool, Bool)> in
+//                            if !isAuthorized {
+//                                print("버튼 토글 값", isOn)
+//                                print("알람 설정 여부", isAuthorized)
+//                                return self.requestNotification().map { (isOn, $0) }
+//                            } else {
+//                                print("버튼 토글 값2", isOn)
+//                                print("알람 설정 여부2", isAuthorized)
+//                                return .just((isOn, isAuthorized))
+//                            }
+//                        }
+//                } else {
+//                    print("====버튼 토글 x")
+//                    return self.checkNotificationSetting()
+//                        .flatMapLatest { isAuthorized -> Observable<(Bool, Bool)> in
+//                            if !isAuthorized {
+//                                print("버튼 토글 값3", isOn)
+//                                print("알람 설정 여부3", isAuthorized)
+//                                return .just((isOn, isAuthorized))
+//                            } else {
+//                                print("알람은 켜져있는데 버튼 토글은 안된 상황")
+//                                // 알림 설정을 끄는 기능을 추가 해야한다.
+//                                print("허용 여부", isAuthorized)
+//                                return .just((isOn, isAuthorized))
+//                            }
+//                        }
+//                }
+//            }
+        
+        return Output(
+            returnTapped: input.returnTapped.asObservable(),
+            displayAlert: isPermissionOn
+        )
     }
 }
