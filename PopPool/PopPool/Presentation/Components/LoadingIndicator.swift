c689944654f9ec58d5d389f7c849d648d5c87d17
Madman-dev <dongjunlee222@gmail.com>
2024-09-03 17:49:13 +0900

    FEAT: ✨ indicator 생성

---
+++ PopPool/PopPool/Presentation/Components/LoadingIndicator.swift
@@ -0,0 +1,50 @@
+//
+//  LoadingIndicator.swift
+//  PopPool
+//
+//  Created by Porori on 9/3/24.
+//
+
+import UIKit
+import Lottie
+import SnapKit
+
+final class LoadingIndicator: UIView {
+    
+    private let animationView: LottieAnimationView = {
+        let view = LottieAnimationView(name: Constants.lottie.loading)
+        return view
+    }()
+    
+    init() {
+        super.init(frame: .zero)
+        setUp()
+        setUpConstraint()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUp() {
+        self.backgroundColor = .clear
+    }
+    
+    private func setUpConstraint() {
+        self.addSubview(animationView)
+        animationView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    public func start() {
+        animationView.play()
+        animationView.loopMode = .loop
+    }
+    
+    public func stop() {
+        self.removeFromSuperview()
+        animationView.stop()
+        animationView.removeFromSuperview()
+    }
+}
