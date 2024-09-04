786bc06323fe3f03258132690ca3fde4d4d4e986
Madman-dev <dongjunlee222@gmail.com>
2024-09-04 15:57:41 +0900

    FIX: 🔧 tableview로 UI 변경 및 데이터 구조 변경

--- PopPool/PopPool/Presentation/Scene/MyPage/SignOut/SignOutVC.swift
+++ PopPool/PopPool/Presentation/Scene/MyPage/SignOut/SignOutVC.swift
@@ -14,16 +14,54 @@ final class SignOutVC: BaseViewController {
     
     // MARK: - Components
     
-    private let headerView = HeaderViewCPNT(title: "회원탈퇴", style: .icon(UIImage(systemName: "lasso")))
-    private lazy var signOutView = SignOutSurveyView(surveyDetails: [])
-    private let scrollView = UIScrollView()
-    private let containerView = UIView()
-    private let contentStackView = UIStackView()
-            
+    private let headerView = HeaderViewCPNT(
+        title: "회원탈퇴",
+        style: .icon(
+            UIImage(systemName: "lasso")))
+    
+    private lazy var headerStack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.addArrangedSubview(tableHeaderView)
+        stack.addArrangedSubview(headerSpaceView)
+        return stack
+    }()
+    
+    private let tableHeaderView = ContentTitleCPNT(
+        title: "탈퇴하려는 이유가\n무엇인가요?",
+        type: .title_sub_fp(
+            subTitle: "알려주시는 내용을 참고해 더 나은 팝풀을\n만들어볼게요."))
+        
+    private var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero)
+        tableView.separatorStyle = .none
+        tableView.register(SignOutTableViewCell.self,
+                           forCellReuseIdentifier: SignOutTableViewCell.identifier)
+        return tableView
+    }()
+    
+    let skipButton = ButtonCPNT(
+        type: .secondary,
+        title: "건너뛰기")
+    let confirmButton = ButtonCPNT(
+        type: .primary,
+        title: "확인")
+    private lazy var buttonStack: UIStackView = {
+        let stack = UIStackView()
+        stack.addArrangedSubview(skipButton)
+        stack.addArrangedSubview(confirmButton)
+        stack.spacing = 12
+        stack.distribution = .fillEqually
+        return stack
+    }()
+    
+    private let headerSpaceView = UIView()
+    
     // MARK: - Properties
     
     private let viewModel: SignOutVM
     private let disposeBag = DisposeBag()
+    private let cellTapSubject = PublishSubject<(Survey, SignOutTableViewCell.SignOutState)>()
     
     // MARK: - Initializer
     
@@ -54,97 +92,91 @@ private extension SignOutVC {
     // MARK: - Method
     
     func setUp() {
-        scrollView.showsVerticalScrollIndicator = false
         self.headerView.rightBarButton.isHidden = true
         self.navigationController?.navigationBar.isHidden = true
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     func setUpConstraints() {
-        view.addSubview(scrollView)
-        scrollView.addSubview(containerView)
-        
-        scrollView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-        }
-        
-        containerView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-            make.width.equalTo(view.snp.width)
-        }
-        
-        containerView.addSubview(headerView)
-        containerView.addSubview(contentStackView)
-        contentStackView.addArrangedSubview(signOutView)
+        view.addSubview(headerView)
         
         headerView.snp.makeConstraints { make in
             make.leading.trailing.top.equalToSuperview()
         }
         
-        contentStackView.snp.makeConstraints { make in
-            make.top.equalTo(headerView.snp.bottom)
-            make.leading.trailing.equalToSuperview().inset(Constants.spaceGuide.small200)
-            make.bottom.equalTo(containerView.snp.bottom)
+        view.addSubview(buttonStack)
+        buttonStack.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.bottom.equalToSuperview().inset(Constants.spaceGuide.medium400)
+            make.height.equalTo(52)
         }
         
-        scrollView.layoutIfNeeded()
-        adjustBehavior()
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(headerView.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalTo(buttonStack.snp.top)
+        }
+        
+        tableView.tableHeaderView = self.headerStack
+        headerStack.layoutMargins = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+        headerStack.isLayoutMarginsRelativeArrangement = true
+        headerStack.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: 250)
+        
+        headerStack.snp.makeConstraints { make in
+            make.width.equalTo(view.bounds.width)
+        }
+        
+        headerSpaceView.snp.makeConstraints { make in
+            make.height.equalTo(Constants.spaceGuide.medium400)
+        }
     }
     
     func bind() {
-        let input = SignOutVM.Input()
+        let input = SignOutVM.Input(
+//            cellTapped: cellTapSubject.asObservable()
+        )
         let output = viewModel.transform(input: input)
         
         output.surveylist
-            .observe(on: MainScheduler.instance)
             .withUnretained(self)
-            .subscribe(onNext: { (owner, list) in
-                owner.signOutView.updateSurvey(list)
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { (owner, data) in
+                self.tableView.reloadData()
             })
             .disposed(by: disposeBag)
-        
-        headerView.leftBarButton.rx.tap
-            .withUnretained(self)
-            .subscribe { (owner, _) in
-                owner.navigationController?.popViewController(animated: true)
-            }
-            .disposed(by: disposeBag)
-        
-        signOutView.tappedValues
-            .withUnretained(self)
-            .subscribe(onNext: { (owner, tapped) in
-                
-                if let last = owner.signOutView.surveyView.last {
-                    last.isCheck
-                        .subscribe { isChecked in
-                            owner.signOutView.makeTextViewActive(isChecked)
-                        }
-                        .disposed(by: owner.disposeBag)
+//        
+//        output.selectedSurvey
+//            .subscribe(onNext: { selected in
+//                print("선택된 값", selected)
+//            })
+//            .disposed(by: disposeBag)
+    }
+}
+
+extension SignOutVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.survey.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: SignOutTableViewCell.identifier, for: indexPath) as? SignOutTableViewCell else { return UITableViewCell() }
+        cell.configure(data: viewModel.survey[indexPath.row])
+        cell.tapSubject
+            .subscribe(onNext: { [weak self] survey, state in
+                if state == .tapped {
+                    self?.viewModel.selectedArray.append(survey)
+                } else {
+                    self?.viewModel.selectedArray.removeAll(where: { $0 == survey })
                 }
             })
             .disposed(by: disposeBag)
         
-        signOutView.skipButton.rx.tap
-            .withUnretained(self)
-            .subscribe { (owner, _) in
-                print("화면 이동 필요")
-            }
-            .disposed(by: disposeBag)
-        
-        signOutView.confirmButton.rx.tap
-            .withUnretained(self)
-            .subscribe { (owner, _) in
-                print("다음 화면 이동")
-                let vc = SignOutCompleteVC()
-                owner.navigationController?.pushViewController(vc, animated: true)
-            }
-            .disposed(by: disposeBag)
+        return cell
     }
     
-    /// scrollView 높이가 contentview보다 낮을 경우 scrollable 기능 활성화 여부
-    func adjustBehavior() {
-        let contentHeight = scrollView.contentSize.height
-        let scrollViewHeight = scrollView.bounds.height
-        
-        scrollView.isScrollEnabled = contentHeight > scrollViewHeight
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
     }
 }
--- PopPool/PopPool/Presentation/Scene/MyPage/SignOut/SignOutVM.swift
+++ PopPool/PopPool/Presentation/Scene/MyPage/SignOut/SignOutVM.swift
@@ -13,33 +13,40 @@ final class SignOutVM: ViewModelable {
     var disposeBag = DisposeBag()
     
     struct Input {
-        
+//        let cellTapped: Observable<(IndexPath, String?)>
     }
     
     struct Output {
-        var surveylist: Observable<[String]>
+        let surveylist: Observable<[Survey]>
+//        let selectedSurvey: Observable<Set<String>>
     }
     
     private let useCase: UserUseCase
+    private var fetchedSurvey = PublishSubject<[Survey]>()
+    var survey: [Survey] = []
+    var selectedArray: [Survey] = []
     
     init() {
         self.useCase = AppDIContainer.shared.resolve(type: UserUseCase.self)
     }
     
     func transform(input: Input) -> Output {
-        let fetchedSurvey = PublishSubject<[String]>()
         
+        // 서베이 데이터를 API를 통해 호출해 옵니다
         useCase.fetchWithdrawlSurveryList()
             .map { response in
-                return response.withDrawlSurveyList.map { $0.survey }
+                return response.withDrawlSurveyList.map { $0 }
             }
-            .subscribe(onNext: { surveyName in
-                fetchedSurvey.onNext(surveyName)
+            .withUnretained(self)
+            .subscribe(onNext: { (owner, response) in
+                owner.survey = response // viewModel의 데이터
+                owner.fetchedSurvey.onNext(response) // vc이 따를 수 있는 이벤트
             })
             .disposed(by: disposeBag)
-        
+                
         return Output(
             surveylist: fetchedSurvey.asObservable()
+//            selectedSurvey: selectedSurvey.asObservable()
         )
     }
 }
