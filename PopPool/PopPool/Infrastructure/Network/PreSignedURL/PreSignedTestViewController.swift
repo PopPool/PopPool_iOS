7f56f12dbb669934efa237e5ca38f1ddff485961
JunYoung <ghddns34@gmail.com>
2024-09-06 23:25:00 +0900

    [FEAT]: PresignedURL Loader, uploader 구현

--- PopPool/PopPool/Infrastructure/Network/PreSignedURL/PreSignedTestViewController.swift
+++ PopPool/PopPool/Infrastructure/Network/PreSignedURL/PreSignedTestViewController.swift
@@ -9,17 +9,53 @@ import UIKit
 
 import RxSwift
 import RxCocoa
+import SnapKit
 
 class PreSignedTestViewController: BaseViewController {
     let service = PreSignedService()
     let disposeBag = DisposeBag()
+    let imageView = UIImageView()
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
-        service.upload(request: .init(objectKeyList: ["filePathTest/Test"]))
-            .subscribe { response in
-                print(response)
+        
+        service.tryUpload(datas: [
+            .init(filePath: "TestPath/TestFile1", image: UIImage(systemName: "folder")!),
+            .init(filePath: "TestPath/TestFile2", image: UIImage(systemName: "lasso")!),
+        ])
+            .subscribe { _ in
+                print("성공!!!!!")
+            } onFailure: { _ in
+                print("실패 ㅠㅠㅠㅠㅠㅠ")
             }
             .disposed(by: disposeBag)
+
+//        service.getUploadLinks(request: .init(objectKeyList: ["filePathTest/Test"]))
+//            .subscribe { response in
+//                print(response)
+//            }
+//            .disposed(by: disposeBag)
+        
+        view.addSubview(imageView)
+        imageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        service.tryDownload(filePaths: [
+            "TestPath/TestFile",
+            "TestPath/TestFile1",
+            "TestPath/TestFile2"
+        ])
+            .subscribe { images in
+                print("성공 !!!!!!!")
+                print(images)
+                if let image = images.last {
+                    self.imageView.image = image
+                }
+            } onFailure: { error in
+                print("실패 ㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠ")
+            }
+            .disposed(by: disposeBag)
+
     }
 }
