f77b4d96d9a910c026dbb6de754c98f44d6a2228
JunYoung <112812473+dongglehada@users.noreply.github.com>
2024-09-06 23:26:13 +0900

    [MREGE]: PresignedURL loader, uploader (#119)
    * [FEAT]: DTO추가 및 파일 생성
    
    * [FEAT]: PresignedURL Loader, uploader 구현

---
+++ PopPool/PopPool/Infrastructure/Network/PreSignedURL/PreSignedService.swift
@@ -0,0 +1,162 @@
+//
+//  PreSignedService.swift
+//  PopPool
+//
+//  Created by SeoJunYoung on 9/5/24.
+//
+
+import Foundation
+import UIKit
+
+import RxSwift
+import RxCocoa
+import Alamofire
+
+class PreSignedService {
+    
+    struct PresignedURLRequest {
+        var filePath: String
+        var image: UIImage
+    }
+    
+    let tokenInterceptor = TokenInterceptor()
+    let disposeBag = DisposeBag()
+    
+    func tryUpload(datas: [PresignedURLRequest]) -> Single<Void> {
+        return Single.create { [weak self] observer in
+            guard let self = self else {
+                return Disposables.create()
+            }
+            
+            getUploadLinks(request: .init(objectKeyList: datas.map { $0.filePath } ))
+                .subscribe { response in
+                    let responseList = response.preSignedUrlList
+                    let inputList = datas
+                    let requestList = zip(responseList, inputList).compactMap { zipResponse in
+                        let urlResponse = zipResponse.0
+                        let inputResponse = zipResponse.1
+                        return self.uploadFromS3(url: urlResponse.preSignedUrl, image: inputResponse.image)
+                    }
+                    print(requestList)
+                    Single.zip(requestList)
+                        .subscribe(onSuccess: { _ in
+                            print("All images uploaded successfully")
+                            observer(.success(()))
+                        }, onFailure: { error in
+                            print("Image upload failed: \(error.localizedDescription)")
+                            observer(.failure(error))
+                        })
+                        .disposed(by: self.disposeBag)
+                } onError: { error in
+                    print("getUploadLinks Fail: \(error.localizedDescription)")
+                    observer(.failure(error))
+                }
+                .disposed(by: disposeBag)
+            return Disposables.create()
+        }
+    }
+    
+    func tryDownload(filePaths: [String]) -> Single<[UIImage]> {
+        return Single.create { [weak self] observer in
+            guard let self = self else {
+                return Disposables.create()
+            }
+            
+            // Download Links 요청
+            self.getDownloadLinks(request: .init(objectKeyList: filePaths))
+                .subscribe { response in
+                    let responseList = response.preSignedUrlList
+                    let requestList = responseList.compactMap { self.downloadFromS3(url: $0.preSignedUrl) }
+                    
+                    // 모든 다운로드 작업을 병렬로 수행
+                    Single.zip(requestList)
+                        .map { dataList -> [UIImage] in
+                            for data in dataList {
+                                print(data.base64EncodedString())
+                            }
+                            return dataList.compactMap { UIImage(data: $0) }
+                        }
+                        .subscribe(onSuccess: { images in
+                            print("All images downloaded successfully")
+                            observer(.success(images))
+                        }, onFailure: { error in
+                            print("Image download failed: \(error.localizedDescription)")
+                            observer(.failure(error))
+                        })
+                        .disposed(by: self.disposeBag)
+                    
+                } onError: { error in
+                    print("getDownloadLinks Fail: \(error.localizedDescription)")
+                    observer(.failure(error))
+                }
+                .disposed(by: self.disposeBag)
+            
+            return Disposables.create()
+        }
+    }
+}
+
+
+private extension PreSignedService {
+    
+    func uploadFromS3(url: String, image: UIImage) -> Single<Void> {
+        return Single.create { single in
+            if let imageData = image.pngData(),
+               let url = URL(string: url)
+            {
+                let request = AF.upload(imageData, to: url, method: .put).response { response in
+                    print("uploadResponse:\(response)")
+                    switch response.result {
+                    case .success:
+                        single(.success(()))
+                    case .failure(let error):
+                        single(.failure(error))
+                    }
+                }
+                
+                return Disposables.create {
+                    request.cancel()
+                }
+            } else {
+                single(.failure(NSError(domain: "InvalidDataOrURL", code: -1, userInfo: nil)))
+                return Disposables.create()
+            }
+        }
+    }
+    
+    func downloadFromS3(url: String) -> Single<Data> {
+        return Single.create { single in
+            if let url = URL(string: url) {
+                let request = AF.request(url).responseData { response in
+                    print("downloadResponse: \(response)")
+                    print(response.value)
+                    switch response.result {
+                    case .success(let data):
+                        single(.success(data))
+                    case .failure(let error):
+                        single(.failure(error))
+                    }
+                }
+                
+                return Disposables.create {
+                    request.cancel()
+                }
+            } else {
+                single(.failure(NSError(domain: "InvalidDataOrURL", code: -1, userInfo: nil)))
+                return Disposables.create()
+            }
+        }
+    }
+    
+    func getUploadLinks(request: PresignedURLRequestDTO) -> Observable<PreSignedURLResponseDTO> {
+        let provider = ProviderImpl()
+        let endPoint = PopPoolAPIEndPoint.presigned_upload(request: request)
+        return provider.requestData(with: endPoint, interceptor: tokenInterceptor)
+    }
+    
+    func getDownloadLinks(request: PresignedURLRequestDTO) -> Observable<PreSignedURLResponseDTO> {
+        let provider = ProviderImpl()
+        let endPoint = PopPoolAPIEndPoint.presigned_download(request: request)
+        return provider.requestData(with: endPoint, interceptor: tokenInterceptor)
+    }
+}
